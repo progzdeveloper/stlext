75a180ab01d6fb592a2189212cfdb8484a155839
progzdeveloper <progzdeveloper@gmail.com>
2020-03-07 01:16:47 +0300

    miscompilations fixed

--- stlext/compability/cxx17/utility.hpp
+++ stlext/compability/cxx17/utility.hpp
@@ -36,45 +36,44 @@
 
 #include "../../platform/common.h"
 
-
+#include "../integer_sequence"
 
 #if (defined(STDX_CMPLR_MSVC) && STDX_CMPLR_MSVC <= 1800) || \
     (defined(STDX_CMPLR_GNU) && (STDX_CMPLR_GNU < 600)) || \
     (defined(STDX_CMPLR_INTEL) && STDX_CMPLR_INTEL <= 1800)
-#include "../integer_sequence"
 
 _STDX_BEGIN
 
 template<class _Container>
-__CONSTEXPR auto inline data(_Container& _Cont)
--> decltype(_Cont.data())
+__CONSTEXPR auto inline data(_Container& c)
+-> decltype(c.data())
 {	// get data() for container
-	return (_Cont.data());
+    return (c.data());
 }
 
 template<class _Container>
-__CONSTEXPR auto inline data(const _Container& _Cont)
--> decltype(_Cont.data())
+__CONSTEXPR auto inline data(const _Container& c)
+-> decltype(c.data())
 {	// get pointer to data of const container
-	return (_Cont.data());
+    return (c.data());
 }
 
 template<class _Ty, size_t _Size>
-inline __CONSTEXPR _Ty *data(_Ty(&_Array)[_Size])
+inline __CONSTEXPR _Ty *data(_Ty(&arr)[_Size])
 {	// get pointer to data of array
-	return (_Array);
+    return (arr);
 }
 
 template<class _Elem> inline
-__CONSTEXPR const _Elem *data(std::initializer_list<_Elem> _Ilist)
+__CONSTEXPR const _Elem *data(std::initializer_list<_Elem> ilist)
 {	// get pointer to data of initializer_list
-	return (_Ilist.begin());
+    return (ilist.begin());
 }
 
 /// C++17 proposal
 template<class C>
-__CONSTEXPR auto size(const C& __c) -> decltype(__c.size()) {
-	return __c.size();
+__CONSTEXPR auto size(const C& c) -> decltype(c.size()) {
+    return c.size();
 }
 
 /// C++17 proposal
