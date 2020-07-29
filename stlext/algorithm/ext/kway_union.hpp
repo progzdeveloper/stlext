134e9103d647e2d229389fd45f693b3493235950
progzdeveloper <progzdeveloper@gmail.com>
2020-07-30 01:12:21 +0300

    kway_union updated

--- stlext/algorithm/ext/kway_union.hpp
+++ stlext/algorithm/ext/kway_union.hpp
@@ -1,5 +1,34 @@
-#ifndef KWAY_UNION_HPP
-#define KWAY_UNION_HPP
+// Copyright (c) 2020, Michael Polukarov (Russia).
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// - Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+// - Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer listed
+//   in this license in the documentation and/or other materials
+//   provided with the distribution.
+//
+// - Neither the name of the copyright holders nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
 
 #include <vector>
 #include <iterator>
@@ -9,8 +38,12 @@
 #include <boost/heap/pairing_heap.hpp>
 #endif
 
+#include "../../platform/common.h"
+
 #include "kway_utility.hpp"
 
+
+_STDX_BEGIN
 
 #ifdef _HAVE_BOOST_LIBS
 
@@ -215,5 +248,4 @@ _OutIt kway_union(const std::vector< _Container >& inputs, _OutIt out) {
     return kway_union(inputs, out, std::less<>{});
 }
 
-
-#endif // KWAY_UNION_HPP
+_STDX_END
